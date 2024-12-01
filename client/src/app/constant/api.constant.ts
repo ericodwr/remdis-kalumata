9df2ad53e47d6747a8a555501fad261e844edc27
ericodwr <ericodr@upnvj.ac.id>
2024-12-01 18:07:13 +0700

    update

--- client/src/app/constant/api.constant.ts
+++ client/src/app/constant/api.constant.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL_IMG = 'http://localhost:8080';
+export const BASE_URL = 'https://remdis-kalumata.net';
+export const BASE_URL_IMG = 'https://remdis-kalumata.net';
