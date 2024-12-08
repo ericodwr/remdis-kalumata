e3b06a50578f04714b149589aefcaadcee91cde2
ericodwr <ericodr@upnvj.ac.id>
2024-12-08 16:23:53 +0700

    fixing endpoint

--- client/src/app/constant/api.constant.ts
+++ client/src/app/constant/api.constant.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://remdis-kalumata.net';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://remdis-kalumata.net';
+// export const BASE_URL = 'http://localhost:8080';
 // export const BASE_URL_IMG = 'https://remdis-kalumata.net';
